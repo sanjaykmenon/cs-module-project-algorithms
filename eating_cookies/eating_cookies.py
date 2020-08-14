c78dac69b716ff245af0ff4f85322ed2bc771a7a
Sanjay Krishna <skp68@mst.edu>
2020-08-13 21:05:51 -0700

    eating cookie small pass

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,7 +4,12 @@ Returns: an integer
 '''
 def eating_cookies(n):
     # Your code here
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
 
+    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
     pass
 
 if __name__ == "__main__":
